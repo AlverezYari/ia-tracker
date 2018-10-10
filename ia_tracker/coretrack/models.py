f5071ab71958c59602e4e11c11126557c54fda17
Casey Phillips <caseyphillips@gmail.com>
2018-10-09 22:10:07 -0500

    Fixed?

--- ia_tracker/coretrack/models.py
+++ ia_tracker/coretrack/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Question(models.campaign):
+    campaign_name = models.CharField(max_length=200)
+    last_saved = models.DateTimeField('date published')
+
+
+class Choice(models.players):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
 # Create your models here.
--- ia_tracker/coretrack/views.py
+++ ia_tracker/coretrack/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hi, this is the IA Tracker!")
 
+def coretr(request):
+    return HttpResponse("Post-Login for Campaign")
+
 # Create your views here.
